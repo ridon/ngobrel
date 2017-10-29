f8f4497f90d3cc0ec39d6827c46fff00d26abc30
Mohammad Anwari <mdamt@mdamt.net>
2017-10-29 22:42:04 +0700

    Embed ShareSecret and DeriveKey to PrivateKey

--- crypto/xeddsa/privatekey.go
+++ crypto/xeddsa/privatekey.go
@@ -2,30 +2,37 @@ package xeddsa
 import (
   "crypto/sha512"
   "encoding/hex"
+  "hash"
+  "github.com/ridon/ngobrel/crypto/x3dh"
   // ED25519 from golang/crypto/x
   "github.com/ridon/ngobrel/crypto/xeddsa/internal/edwards25519"
   "io"
 )
 
-type PrivateKey struct {
-  Contents [Keysize]byte
+type PrivateKey [Keysize]byte
+
+func (t *PrivateKey) Encode() [Keysize + 1]byte {
+  var ret [Keysize+1]byte
+  ret[0] = 0x5
+  copy(ret[1:], t[:])
+  return ret
 }
 
 func (t *PrivateKey) HexString() string{
-  return hex.EncodeToString(t.Contents[:])
+  return hex.EncodeToString(t[:])
 }
 
 func NewPrivateKey(key [Keysize]byte) *PrivateKey {
-  ret := PrivateKey {
-    Contents : key,
-  }
+  ret := PrivateKey(key)
   return &ret
 }
 
 func (t *PrivateKey) GetEd25519PublicKey() [Keysize]byte {
   var A edwards25519.ExtendedGroupElement
 	var publicKey [Keysize]byte
-	edwards25519.GeScalarMultBase(&A, &t.Contents)
+  var slice [Keysize]byte
+  copy(slice[:], t[:])
+  edwards25519.GeScalarMultBase(&A, &slice)
 	A.ToBytes(&publicKey)
 
   return publicKey;
@@ -43,7 +50,7 @@ func (t *PrivateKey) Sign(random io.Reader, message []byte) *[Keysize * 2]byte {
   var hash[Keysize * 2]byte
 	digest := sha512.New()
 	digest.Write(initData[:])
-	digest.Write(t.Contents[:])
+	digest.Write(t[:])
 	digest.Write(message)
 	digest.Write(randomByte[:])
 	digest.Sum(hash[:0])
@@ -68,7 +75,9 @@ func (t *PrivateKey) Sign(random io.Reader, message []byte) *[Keysize * 2]byte {
 	edwards25519.ScReduce(&hramDigestReduced, &hramDigest)
 
   var s [Keysize]byte
-	edwards25519.ScMulAdd(&s, &hramDigestReduced, &t.Contents, &hashReduced)
+  var slice[Keysize]byte
+  copy(slice[:], t[:])
+	edwards25519.ScMulAdd(&s, &hramDigestReduced, &slice, &hashReduced)
 
 	ret := new([Keysize * 2]byte)
 	copy(ret[:], encodedR[:])
@@ -76,5 +85,17 @@ func (t *PrivateKey) Sign(random io.Reader, message []byte) *[Keysize * 2]byte {
 	ret[Keysize * 2 -1] |= edPubKey[Keysize - 1] & 0x80
 
   return ret;
+}
+
+func (t *PrivateKey) ShareSecret(withOther PublicKey) [32]byte {
+  var key [Keysize]byte;
+  copy(key[:], t[:])
+  return x3dh.GenerateSharedSecret(withOther, key)
+}
+
+func (t *PrivateKey) DeriveKey(withOther PublicKey, hashFn func() hash.Hash, info string, length int) ([]byte, error) {
+  shared := t.ShareSecret(withOther)
+
+  return x3dh.KDF(hashFn, shared[:32], info, length)
 }
 
