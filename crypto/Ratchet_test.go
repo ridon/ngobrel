63631c3628d1d4731c500c52723770242a24aaf2
Mohammad Anwari <mdamt@mdamt.net>
2017-11-13 09:11:45 +0700

    Cover late and out-of-order messages cases

--- crypto/Ratchet_test.go
+++ crypto/Ratchet_test.go
@@ -128,6 +128,197 @@ func TestRatchetProto(t *testing.T) {
   }
   fmt.Printf("")
 
+  // ---------------------------------
+  // bob writes again but not delivered 
+
+  msgToBeEncryptedLate1 := []byte("olala2")
+  encLate1, err := bobRatchet.Encrypt(msgToBeEncryptedLate1, ad)
+  if err != nil {
+    t.Error(err)
+  }
+
+  // ---------------------------------
+  // bob writes again 
+
+  msgToBeEncrypted = []byte("olala2")
+  enc, err = bobRatchet.Encrypt(msgToBeEncrypted, ad)
+  if err != nil {
+    t.Error(err)
+  }
+
+  dec, err = aliceRatchet.Decrypt(enc, ad)
+  if err != nil {
+    t.Error(err)
+  }
+
+  if hex.EncodeToString(dec) != hex.EncodeToString(msgToBeEncrypted) {
+    t.Error("Unable to decrypt")
+  }
+
+  // ---------------------------------
+  // bob writes again 
+
+  msgToBeEncrypted = []byte("olala2")
+  enc, err = bobRatchet.Encrypt(msgToBeEncrypted, ad)
+  if err != nil {
+    t.Error(err)
+  }
+
+  dec, err = aliceRatchet.Decrypt(enc, ad)
+  if err != nil {
+    t.Error(err)
+  }
+
+  if hex.EncodeToString(dec) != hex.EncodeToString(msgToBeEncrypted) {
+    t.Error("Unable to decrypt")
+  }
+  // ---------------------------------
+  // bob writes again but not delivered 
+
+  msgToBeEncryptedLate2 := []byte("olala2")
+  encLate2, err := bobRatchet.Encrypt(msgToBeEncryptedLate2, ad)
+  if err != nil {
+    t.Error(err)
+  }
+
+  // --------------------------------
+  // alice replies
+  msgToBeEncrypted = []byte("olala")
+  enc, err = aliceRatchet.Encrypt(msgToBeEncrypted, ad)
+  if err != nil {
+    t.Error(err)
+  }
+
+  dec, err = bobRatchet.Decrypt(enc, ad)
+  if err != nil {
+    t.Error(err)
+  }
+
+  if hex.EncodeToString(dec) != hex.EncodeToString(msgToBeEncrypted) {
+    t.Error("Unable to decrypt")
+  }
+  fmt.Printf("")
+
+  // ---------------------------------
+  // alice writes again but not delivered 
+
+  msgToBeEncryptedLateAlice1 := []byte("olala alice1")
+  encLateAlice1, err := aliceRatchet.Encrypt(msgToBeEncryptedLateAlice1, ad)
+  if err != nil {
+    t.Error(err)
+  }
+
+  // --------------------------------
+  // alice replies
+  msgToBeEncrypted = []byte("olala")
+  enc, err = aliceRatchet.Encrypt(msgToBeEncrypted, ad)
+  if err != nil {
+    t.Error(err)
+  }
+
+  dec, err = bobRatchet.Decrypt(enc, ad)
+  if err != nil {
+    t.Error(err)
+  }
+
+  if hex.EncodeToString(dec) != hex.EncodeToString(msgToBeEncrypted) {
+    t.Error("Unable to decrypt")
+  }
+  fmt.Printf("")
+
+
+  // ---------------------------------
+  // message encLate2 arrived later 
+
+  dec, err = aliceRatchet.Decrypt(encLate2, ad)
+  if err != nil {
+    t.Error(err)
+  }
+
+  if hex.EncodeToString(dec) != hex.EncodeToString(msgToBeEncryptedLate2) {
+    t.Error("Unable to decrypt")
+  }
+
+  // --------------------------------
+  // alice replies
+  msgToBeEncrypted = []byte("olala")
+  enc, err = aliceRatchet.Encrypt(msgToBeEncrypted, ad)
+  if err != nil {
+    t.Error(err)
+  }
+
+  dec, err = bobRatchet.Decrypt(enc, ad)
+  if err != nil {
+    t.Error(err)
+  }
+
+  if hex.EncodeToString(dec) != hex.EncodeToString(msgToBeEncrypted) {
+    t.Error("Unable to decrypt")
+  }
+  fmt.Printf("")
+
+
+
+  // ---------------------------------
+  // message encLate1 arrived later 
+
+  dec, err = aliceRatchet.Decrypt(encLate1, ad)
+  if err != nil {
+    t.Error(err)
+  }
+
+  if hex.EncodeToString(dec) != hex.EncodeToString(msgToBeEncryptedLate1) {
+    t.Error("Unable to decrypt")
+  }
+
+  // --------------------------------
+  // alice replies
+  msgToBeEncrypted = []byte("olala")
+  enc, err = aliceRatchet.Encrypt(msgToBeEncrypted, ad)
+  if err != nil {
+    t.Error(err)
+  }
+
+  dec, err = bobRatchet.Decrypt(enc, ad)
+  if err != nil {
+    t.Error(err)
+  }
+
+  if hex.EncodeToString(dec) != hex.EncodeToString(msgToBeEncrypted) {
+    t.Error("Unable to decrypt")
+  }
+  fmt.Printf("")
+
+  // --------------------------------
+  // alice replies
+  msgToBeEncrypted = []byte("olala")
+  enc, err = aliceRatchet.Encrypt(msgToBeEncrypted, ad)
+  if err != nil {
+    t.Error(err)
+  }
+
+  dec, err = bobRatchet.Decrypt(enc, ad)
+  if err != nil {
+    t.Error(err)
+  }
+
+  if hex.EncodeToString(dec) != hex.EncodeToString(msgToBeEncrypted) {
+    t.Error("Unable to decrypt")
+  }
+  fmt.Printf("")
+
+  // --------------------------------
+  // Bob received AliceLate1
+  dec, err = bobRatchet.Decrypt(encLateAlice1, ad)
+  if err != nil {
+    t.Error(err)
+  }
+
+  if hex.EncodeToString(dec) != hex.EncodeToString(msgToBeEncryptedLateAlice1) {
+    t.Error("Unable to decrypt")
+  }
+  fmt.Printf("")
+
 
 
 }
