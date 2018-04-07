e59ca651917558b317b46f067188ad60f15b288c
Mohammad Anwari <mdamt@mdamt.net>
2018-04-07 21:41:54 +0700

    New test showing how media file can be sent

--- android/core/src/androidTest/java/id/ridon/ngobrel/core/SesameConversationInstrumentedTest.java
+++ android/core/src/androidTest/java/id/ridon/ngobrel/core/SesameConversationInstrumentedTest.java
@@ -33,7 +33,25 @@ public class SesameConversationInstrumentedTest {
   // Every message is put in an array inside a hashmap of device id and encrypted data
   HashMap<HashId, ArrayList<byte[]>> mailBoxes = new HashMap<>();
 
+  // Media
+  HashMap<HashId, byte[]> mediaFiles = new HashMap<>();
+
   public SesameConversationInstrumentedTest() throws NoSuchAlgorithmException, InvalidKeyException {
+  }
+
+
+  // This simulates uploading a file to a URL
+  // Returning an ID string
+  String serverUploadMedia(byte[] media) throws NoSuchAlgorithmException, InvalidKeyException {
+    HashId id = HashId.random();
+    mediaFiles.put(id, media);
+
+    return id.toString();
+  }
+
+  // This simulates downloading a file from a URL
+  byte[] serverDownloadMedia(String url) throws InvalidKeyException {
+    return mediaFiles.get(new HashId(Utils.fromHexString(url)));
   }
 
   void serverPutToMailbox(byte[] encrypted) throws IOException, InvalidKeyException {
@@ -400,5 +418,112 @@ public class SesameConversationInstrumentedTest {
     decrypted = charlieBobConversation.decrypt(download);
     Assert.assertArrayEquals("Hello charlie from bob".getBytes(), decrypted);
   }
+
+  @Test
+  public void testMedia() throws Exception {
+    mailBoxes = new HashMap<>();
+
+    // Alice got a device
+    SesameSenderDevice aliceDevice = new SesameSenderDevice(AliceDeviceId1, AliceUserId);
+
+    // Alice uploads her bundle
+    byte[] aliceBundlePublicRaw = aliceDevice.getBundle().bundlePublic.encode();
+
+    // She sents the AliceDeviceId1 and the above raw data
+    // Server keeps it
+    // according to the device and user
+    BundlePublicCollection aliceBundlePublicCollection = new BundlePublicCollection(AliceDeviceId1, BundlePublic.decode(aliceBundlePublicRaw));
+    serverBundles.put(AliceUserId, aliceBundlePublicCollection);
+
+    // Bob also has a device
+    SesameSenderDevice bobDevice = new SesameSenderDevice(BobDeviceId1, BobUserId);
+
+    // and uploads his
+    byte[] bobBundlePublicRaw = bobDevice.getBundle().bundlePublic.encode();
+
+    // Server then gets it
+    // and collect it by it's device id
+    BundlePublicCollection bobBundlePublicCollection = new BundlePublicCollection(BobDeviceId1, BundlePublic.decode(bobBundlePublicRaw));
+    serverBundles.put(BobUserId, bobBundlePublicCollection);
+
+    // Alice wants to send a message to Bob
+    // She downloads Bob's public bundle
+    // First, the server prepares it first and make it ready to be downloaded
+    BundlePublicCollection serverAliceBundlePublicCollection = serverBundles.get(BobUserId);
+    byte[] download = serverAliceBundlePublicCollection.encode();
+
+    // Alice got Bob's bundle public
+    BundlePublicCollection aliceBobBundlePublicCollection = BundlePublicCollection.decode(download);
+
+    // Alice starts a conversation with Bob
+    SesameConversation aliceConversation = new SesameConversation(AliceUserId, aliceDevice.id, aliceDevice.getBundle(), BobUserId, aliceBobBundlePublicCollection);
+    aliceConversation.initializeSender();
+
+    String message = "alice-msg1";
+
+    byte[] decrypted;
+    byte[] encrypted = aliceConversation.encrypt(new MessageExamplePayload(null, message.getBytes()).encode());
+
+    // The encrypted text is uploaded to server. Server then puts it in a mailbox
+    serverPutToMailbox(encrypted);
+
+    // Alice sends out a media file
+    // This is the meta data we prepare
+    String ad = "MEDIA";
+    String contentType = "image/png";
+    String fileName = "image.png";
+    byte[] image = "PNG.This is an image.blbbblblablabla".getBytes();
+    byte[] key = HashId.random().raw();
+
+    // Then we encrypt the media file
+    Aead mediaEncryptor = new Aead(key, "My Chat App");
+    encrypted = mediaEncryptor.encrypt(image, ad.getBytes());
+
+    // And upload
+    String urlFromServer = serverUploadMedia(encrypted);
+
+    // And send a media payload
+    encrypted = aliceConversation.encrypt(new MessageExamplePayload(null, urlFromServer, contentType, fileName, key).encode());
+
+    // The encrypted text is uploaded to server. Server then puts it in a mailbox
+    serverPutToMailbox(encrypted);
+    // Server then -- in some way -- tells Bob that he has an incoming message
+    // Bob then initiates a conversation on his side
+    // He does that by downloading Alice's bundle
+    BundlePublicCollection serverBobBundlePublicCollection = serverBundles.get(AliceUserId);
+    download = serverBobBundlePublicCollection.encode();
+
+    BundlePublicCollection bobAliceBundlePublicCollection = BundlePublicCollection.decode(download);
+
+    SesameConversation bobConversation = new SesameConversation(BobUserId, bobDevice.id, bobDevice.getBundle(), AliceUserId, bobAliceBundlePublicCollection);
+
+    // Bob downloads all the messages from bobDevice.id
+
+    download = serverFetchEncrypted(bobDevice.id);
+    Assert.assertNotNull(download);
+    decrypted = bobConversation.decrypt(download);
+    MessageExamplePayload payload = MessageExamplePayload.decode(decrypted);
+    Assert.assertEquals(payload.type, 0); // This is a plain message
+    Assert.assertEquals(Arrays.equals(payload.contents, message.getBytes()), true);
+
+    download = serverFetchEncrypted(bobDevice.id);
+    Assert.assertNotNull(download);
+    decrypted = bobConversation.decrypt(download);
+    payload = MessageExamplePayload.decode(decrypted);
+    Assert.assertEquals(payload.type, 1); // This is a media message
+
+    // Lets decrypt the message first by getting it from the URL it specified
+    String urlFromPayload = payload.url;
+    byte[] mediaFromServer = serverDownloadMedia(urlFromPayload);
+
+    // We got the media, now decrypt it using the key from the payload
+    Aead aeadBob = new Aead(payload.contents, "My Chat App");
+    decrypted = aeadBob.decrypt(mediaFromServer, ad.getBytes()); // ad and info must be exactly the same
+
+    // Check whether the decrypted is really the same with the image Alice sent in the first place
+    Assert.assertEquals(Arrays.equals(decrypted, image), true);
+
+  }
+
 
 }
