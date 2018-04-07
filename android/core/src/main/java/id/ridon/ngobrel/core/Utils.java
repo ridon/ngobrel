4db3ec88c1afed9de87fffe8cc23010d1a3f8c28
Mohammad Anwari <mdamt@mdamt.net>
2018-04-07 21:41:34 +0700

    New fromHexString util function

--- android/core/src/main/java/id/ridon/ngobrel/core/Utils.java
+++ android/core/src/main/java/id/ridon/ngobrel/core/Utils.java
@@ -11,4 +11,14 @@ class Utils {
     }
     return new String(hexChars);
   }
+
+  public static byte[] fromHexString(String s) {
+    int length = s.length();
+    byte[] data = new byte[length / 2];
+    for (int i = 0; i < length; i += 2) {
+      data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4) +
+                             Character.digit(s.charAt(i+1), 16));
+    }
+    return data;
+  }
 }
